8ed7b3ec05bced381214092daf9b709446f70e37
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-10-08 21:39:08 +0200

    Created formDate function

---
+++ src/app.js
@@ -0,0 +1,23 @@
+function formDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
